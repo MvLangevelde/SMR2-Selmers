580022d340b4cbdc203d1ffdc1626e7f97235ed0
WouterLettinga <57904584+WouterLettinga@users.noreply.github.com>
2020-01-14 10:42:56 +0100

    Update Spiral_weld_0.py

--- Spiral_weld_0.py
+++ Spiral_weld_0.py
@@ -1,4 +1,4 @@
- oo#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Dec 13 18:02:54 2019
@@ -8,211 +8,126 @@ Created on Fri Dec 13 18:02:54 2019
 import numpy as np
 import math 
 from urx import Robot
+import serial
 
 robot = Robot("192.168.0.20", True)
+arduino = serial.Serial('/dev/cu.usbmodem144201', 9600)
 
-def spiralweld(x_p, y, z, it, r, pos):
-
-    def teta(x,y):
-        if y == 0:
-            return 0
-        teta = math.atan(x/y)
-        return teta
+def spiralcoordinates(start, weld, radius1):
     
-    Xref, Yref, Zref = [0,r,0]
-    Xmes, Ymes, Zmes = [x_p,y,z]
+    radius = radius1 / 1000
     
-    Xref_cil = (Xref**2+Yref**2)**0.5
-    Yref_cil = teta(Xref, Yref)
-    Zref_cil = Zref
+    theta = np.linspace(0, 2*np.pi, 1000)
     
-    Xmes_cil = (Xmes**2+Ymes**2)**0.5
-    Ymes_cil = teta(Xmes, Ymes)
-    Zmes_cil = Zmes
+    radius = np.sqrt(radius**2)
     
-    def cylindercoords():
-        x_l = np.linspace(Xref_cil,Xmes_cil,it)
-        y_l = np.linspace(Yref_cil, Ymes_cil, it)
-        z_l = np.linspace(Zref_cil, Zmes_cil, it)
+    xr = radius*np.cos(theta)
+    yr = radius*np.sin(theta)
     
-        return x_l,y_l,z_l
+    x_cir1 = xr[0:250]
+    y_cir1 = yr[0:250]
+    x_cir = x_cir1[::-1]
+    y_cir = y_cir1[::-1]
     
-    Xc = cylindercoords()[0]
-    Yc = cylindercoords()[1]
-    Zc = cylindercoords()[2]
+    x = abs(start[0] - weld[0]) 
+    y = abs(start[1] - weld[1])
+    z = radius - abs(start[2] - weld[2]) 
     
-    def cartesiancoords():
-        x_l = []
-        y_l = []
-        z_l = Zc
-        
-        for i in range(0,it):
-            x_l1 = r*math.cos(Yc[i]) 
-            y_l1 = r*math.sin(Yc[i])
+    print('Spiralweld coordinate =', [x, y, z])
     
-            x_l.append(x_l1)
-            y_l.append(y_l1)
-        
-        return x_l, y_l, z_l
+    length = int(250 - ((math.atan(z/y)) * (125 / (0.25 * math.pi))))
+         
+    print('Length of quarter circle =', length)
+    print('Angle of rx =', abs(math.pi-abs(weld[3])) + math.pi )
+    print('-------------------------')
     
-    Xf = cartesiancoords()[0] 
-    Yf = cartesiancoords()[1]
-    Zf = cartesiancoords()[2]
-        
-    def arcmove():        
-        x = pos[0]
-        y = pos[1]
-        z = pos[2]
-        rx = math.pi
+    x_weld = x_cir[0:length]
+    y_weld = y_cir[0:length]
+    
+    pose_list = []
+    
+    rx_list = np.linspace(start[3], abs(math.pi-abs(weld[3])) + math.pi, length) #((y / radius)+1) * math.pi, length)
+    
+    x_list = np.linspace(start[0], weld[0], length)
+    
+    for i in range(0, length):
+          
+        x = x_list[i] 
+        y = start[1] - (x_weld[i] - x_weld[0])
+        z = start[2] + (y_weld[i] - y_weld[0])
+        rx = rx_list[i]
         ry = 0
         rz = 0
-        
-        pose_list = []
-        
-        for i in range(it):
-            x = pos[0] - (Zf[i]/1000)
-            y = pos[1] + (Yf[i]/1000)
-            z = pos[2] + (Xf[i]/1000) - (Xf[0]/1000)
-            rx += (((x_p-20) / r) * (- 0.5 * math.pi))/it
-            
-            pose_list += [[x, y, z, rx, ry, rz]]
-         
-        return pose_list
-    
-    return arcmove()
 
-def spiralweldback(x_p, y, z, it, r, pos):
-    pose_list = spiralweld(x_p, y, z, it, r, pos)
+        pose_list += [[x, y, z, rx, ry, rz]]
+                   
+    return pose_list   
+
+def spiralback(start, weld, radius1):
+    pose_list = spiralcoordinates(start, weld, radius1)
     pose_list_back = pose_list[::-1]
     return pose_list_back
 
-def quartercircle(r, pos, iterated):
+def quartercircle(r, pos):
     
-    theta = np.linspace(0, 2*np.pi, 200)
+    print('Searching for weld......')
+    print('-------------------------')
+    
+    theta = np.linspace(0, 2*np.pi, 1000)
     
     radius = np.sqrt(r**2)
     
     x = radius*np.cos(theta)
     y = radius*np.sin(theta)
     
-    x_cir1 = x[0:50]
-    y_cir1 = y[0:50]
+    x_cir1 = x[0:250]
+    y_cir1 = y[0:250]
     x_cir = x_cir1[::-1]
     y_cir = y_cir1[::-1]
     
     pose_list = []
     
-    rx_list = np.linspace(pos[3], 0.5*math.pi, len(x_cir))
+    rx_list = np.linspace(abs(pos[3]), 1.5*math.pi, len(x_cir))
     
     for i in range(0,len(y_cir)):
+    
         x = pos[0]
-        y = pos[1] + x_cir[i]/1000 
+        y = pos[1] - x_cir[i]/1000 
         z = pos[2] + (y_cir[i]/1000 - y_cir[0]/1000)
         rx = rx_list[i]
-        ry = pos[4]
-        rz = pos[5]
-
-        pose_list += [[x, y, z, rx, ry, rz]]
-        
-        if iterated == 'iterated':
-            robot.movel([x, y, z, rx, ry, rz], vel = 1, acc = 0.2)
-
-    return pose_list
-
-def quartercircleback(r, pos):
-    pose_list = quartercircle(r, pos, 'false')
-    pose_list_back = pose_list[::-1]
-    return pose_list_back
-
-def spiralweldback(x_p, y, z, it, r, pos):
-
-    def teta(x,y):
-        if y == 0:
-            return 0
-        teta = math.atan(x/y)
-        return teta
-    
-    Xref, Yref, Zref = [0,r,0]
-    Xmes, Ymes, Zmes = [x_p,y,z]
-    
-    Xref_cil = (Xref**2+Yref**2)**0.5
-    Yref_cil = teta(Xref, Yref)
-    Zref_cil = Zref
-    
-    Xmes_cil = (Xmes**2+Ymes**2)**0.5
-    Ymes_cil = teta(Xmes, Ymes)
-    Zmes_cil = Zmes
-    
-    def cylindercoords():
-        x_l = np.linspace(Xref_cil,Xmes_cil,it)
-        y_l = np.linspace(Yref_cil, Ymes_cil, it)
-        z_l = np.linspace(Zref_cil, Zmes_cil, it)
-    
-        return x_l,y_l,z_l
-    
-    Xc = cylindercoords()[0]
-    Yc = cylindercoords()[1]
-    Zc = cylindercoords()[2]
-    
-    def cartesiancoords():
-        x_l = []
-        y_l = []
-        z_l = Zc
-        
-        for i in range(0,it):
-            x_l1 = r*math.cos(Yc[i]) 
-            y_l1 = r*math.sin(Yc[i])
-    
-            x_l.append(x_l1)
-            y_l.append(y_l1)
-        
-        return x_l, y_l, z_l
-    
-    Xf = cartesiancoords()[0] 
-    Yf = cartesiancoords()[1]
-    Zf = cartesiancoords()[2]
-        
-    def arcmove():        
-        x = pos[0]
-        y = pos[1]
-        z = pos[2]
-        rx = math.pi
         ry = 0
         rz = 0
-        
-        pose_list = []
-        
-        rx_list = np.linspace(pos[3], 0.5*math.pi, it)
-        
-        for i in range(it):
-            x = pos[0] - (Zf[i]/1000)
-            y = pos[1] + (Yf[i]/1000)
-            z = pos[2] + (Xf[i]/1000) - (Xf[0]/1000)
-            rx = rx_list[i]
+
+        pose_list += [[x, y, z, rx, ry, rz]]
+                           
+    return pose_list
+
+def arduinocheck():
             
-            pose_list += [[x, y, z, rx, ry, rz]]
-         
-        return pose_list
+    print('Checking arduino......')
+    print('-------------------------')
     
-    return arcmove()
+    for i in range(0,30):
+        arduino = serial.Serial('/dev/cu.usbmodem144201', 9600)
+        data = arduino.readline().decode(encoding="utf-8", errors='ignore').rstrip("\r\n")
+        i+=1
+    return data
 
+def spiralcoords(start, weldpos):
+    x = abs(start[1] - weldpos[1]) * 1000
+    y = abs(start[2] - weldpos[2]) * 1000
+    z = abs(start[0] - weldpos[0]) * 1000
+    return x, y, z
 
-    
-    
-    
+def induxion():  
+    data1 = arduino.readline().decode(encoding="utf-8", errors='ignore').rstrip("\r\n")      
+    data =  str.split(data1)
+    indux = int(data[0])
+    return indux
 
-
-
-
-
-
-
-
-
-
-    
-    
-        
-        
-
-
+def switch():
+    data1 = arduino.readline().decode(encoding="utf-8", errors='ignore').rstrip("\r\n")
+    data =  str.split(data1)
+    switch = int(data[1])
+#    print('End switch data =', switch)
+    return switch
