f7f7c0dd29c1277442145b5592c9bafe1770a928
MvLangevelde <58216195+MvLangevelde@users.noreply.github.com>
2020-01-23 23:25:03 +0100

    Added the movements for the whole process

--- HMI.py
+++ HMI.py
@@ -63,38 +63,56 @@ class Control(Screen):
     def Start(self):
         print(self.log_string)
         if ("The diameter is set" in self.log_string):
-#            self.robot.set_tcp([0,0,0,0,0,0])
-            self.robot.movel(self.start)#, vel = 2.0, acc = 1.0)
+            
+            #Startup & homing
             if self.state == 0:
+                self.robot.set_analog_out(0, 0.0)
+                self.robot.wet_analog_out(1, 0.0)
+                self.robot.set_tool_voltage(0)
+                self.robot.movel(self.start, vel = 1.0, acc = 1.0)
                 self.state = 1
                 self.Start()
+            
+            #Go to pipe
             elif self.state == 1:
-                sw.arduinocheck()
                 self.log_string += "Homing\n"
                 self.Refresh()
-                self.robot.movel([0, 0, -0.500, 0, 0, 0], relative = True, wait = False, vel = 0.003, acc = 1)
+                self.robot.movel([0, 0, -0.500, 0, 0, 0], relative = True, wait = False, vel = 0.007, acc = 1)
                 Clock.schedule_interval(self.CheckForPipe,1/1000)
+                
+            #Looking for weld
             elif self.state == 2:
                 sw.arduinocheck()
                 self.robot.set_analog_out(0, 0.5)
                 Clock.schedule_interval(self.CheckForWeld,1/1000)
+                
+            #Looking for direction
             elif self.state == 3:
-                sw.arduinocheck()
-                self.robot.movel([0, 0, 0.01, 0, 0, 0], relative = True)
-                self.robot.movel([-0.050, 0, 0, 0, 0, 0], relative = True)
-                self.robot.movel([0, 0, -0.01, 0, 0, 0], relative = True)
+                self.robot.movel([0, 0, 0.03, 0, 0, 0], relative = True)
+                self.robot.movel([-0.080, 0, 0, 0, 0, 0], relative = True, wait = True)
+                self.robot.movel([0, 0, -0.03, 0, 0, 0], relative = True)
                 self.start_circle = self.robot.getl()
-                print(self.start_circle)
+                sw.arduinocheck()
                 self.robot.movels(sw.quartercircle(self.r, self.start_circle), wait = False)
                 Clock.schedule_interval(self.WeldDirection,1/1000)
+                
+            #Set grinder on pipe
             elif self.state == 4:
+                self.robot.movel([0, 0, self.d_y/1000 + 0.080, 0, 0, 0], relative = True)
+                self.robot.movel(self.startgrinder, vel = 2.0, acc = 1.0)
+                self.robot.movels(sw.quartercircle(self.startgrinder), wait = False)
+                time.sleep(3)
+                self.robot.stop()
+                self.robot.movel([0, 0, -0.500, 0, 0, 0], relative = True, wait = False, vel = 0.007, acc = 1)
                 Clock.schedule_interval(self.Set,1/1000)
+            
+            #Grinding
             elif self.state == 5:
                 self.Grinding()
+            
         elif ("The diameter is set" not in self.log_string and self.state == 0):
             self.log_string = "No diameter set"
             self.Refresh()
-        print(self.state)
         
     def Stop(self):
         if self.state != 0:
@@ -116,8 +134,10 @@ class Control(Screen):
             self.Refresh()
         if sw.switch() <= 3:
             self.robot.stop()
-            self.start = self.robot.getl()
             Clock.unschedule(self.CheckForPipe)
+            self.robot.movel([0, 0, -0.007, 0, 0, 0], relative = True, vel = 0.003, acc = 1)
+            self.start = self.robot.getl()
+            self.startgrinder = sw.sensor_grind_coords(self.start)
             self.state = 2
             self.log_string += "Tool on pipe\n"
             self.Refresh()
@@ -142,14 +162,15 @@ class Control(Screen):
         if sw.induxion() <= 3:
             self.robot.stop()
             self.welpos = self.robot.getl()
-            d_x, self.d_y, d_z = sw.spiralcoords(self.start, self.welpos)
+            self.welposgrinder = sw.sensor_grind_coords(self.welpos)
+            self.d_x, self.d_y, d_z = sw.spiralcoords(self.start, self.welpos)
             Clock.unschedule(self.WeldDirection)               
-            if (d_x >= -0.05 and d_x <= 0.05):
+            if (self.d_x >= -0.05 and self.d_x <= 0.05):
                 self.state = 4
                 self.log_string += "Direction of weld is parallel to axis of rotation\n"
-            elif d_x > 0.05:
+            elif self.d_x > 0.05:
                 self.state = 4
-                self.log_string += "Direction of weld has angle to axis of rotation\nAngle is x degrees\n"
+                self.log_string += "Direction of weld has angle to axis of rotation\n"
             self.Refresh()
             self.Start()
             
@@ -157,21 +178,24 @@ class Control(Screen):
         if "Tuning on pipe\n" not in self.log_string:
             self.log_string += "Tuning on pipe\n"
             self.Refresh()
-        #movement to pipe
-        Clock.unschedule(self.Set)
-        self.state = 5
-        self.log_string += "Tool tuned"
-        self.Refresh()        
-        self.Start()
+        if sw.endstop_grinder() <= 50:
+            self.robot.stop()
+            Clock.unschedule(self.Set)
+            self.robot.set_tool_voltage(24)
+            self.state = 5
+            self.log_string += "Tool tuned\n"
+            self.Refresh()
+            self.Start()
     
     def Grinding(self,dt):
         if "Grinding in progress\n" not in self.log_string:
             self.log_string += "Grinding in progress\n"
             self.Refresh()
-        self.robot.movel([0,0,self.y/1000,0,0,0], relative = True)#, vel = 2.0, acc = 1.0)
-        self.robot.movel(self.start)#, vel = 2.0, acc = 1.0)
-        self.robot.movels(sw.spiralcoordinates(self.start, self.welpos, self.r))
-        self.robot.movels(sw.spiralback(self.start, self.welpos, self.r))
+        self.robot.movel([0, 0, 0.03, 0, 0, 0], relative = True)#, vel = 2.0, acc = 1.0)
+        self.robot.movel([0, 0 + self.d_x, 0, 0, 0, 0], relative = True)
+        self.robot.movel(self.welposgrinder)
+        self.robot.movels(sw.spiralback(self.startgrinder, self.welposgrinder, self.r), vel = 0.03, acc = 1)
+        self.robot.movel([0.050,0,0,0,0,0], relative = True)        
         self.state = 0
         self.log_string += "Finished\n-------------------------" 
         self.Refresh()
